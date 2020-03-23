a60e1e48f85c325e26fec4d7d5c706f23d522c26
shwethasoma <58771999+shwethasoma@users.noreply.github.com>
2020-03-23 15:10:00 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('nodejs ') {
+job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
